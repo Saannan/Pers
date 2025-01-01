44312dd1b7cc2fe51a10728155223cafe36517a4
Saannan <sanjayaadiputra376@gmail.com>
2025-01-01 20:36:16 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+{
+  "name": "V-Tube Downloader",
+  "version": "1.0.0",
+  "scripts": {
+    "start": "vercel dev"
+  },
+  "dependencies": {
+    "axios": "^1.7.8",
+    "express": "^4.19.1"
+  },
+  "type": "module"
+    }
